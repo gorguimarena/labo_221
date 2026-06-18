bd48386e8dbfa18b9f6549201f10519d74abf3c0
gorguimarena <gorguimarena@gmail.com>
2026-06-18 14:48:06 +0000

    correction erreur valeur keys

--- src/models/keys.models.js
+++ src/models/keys.models.js
@@ -1,6 +1,6 @@
 export const keys = {
-    analyse,
-    patient,
-    prelevement,
-    technicien
+    analyse: "analyse",
+    patient: "patient",
+    prelevement: "prelevement",
+    technicien: "technicien"
 }
